9f927064d41a30b90cccf5c8e8ced560dd183508
CrayZiIx <118490456+CrayZiIx@users.noreply.github.com>
2023-07-08 14:24:03 +0200

    add test for gnl

---
+++ test.c
@@ -0,0 +1,17 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+
+
+int main(void)
+{
+    char    *line;
+    int fd;
+
+    fd = open("blabla.txt", O_RDONLY);
+    while((line = get_next_line(fd)))
+    {    
+        printf("-----------\n");
+        printf("-<%s>-\n", line);
+    }
+    return (0);
+}
